709809c39afd3459f7307d454ed61263d599e8ee
roberteftene <roberteftene25@gmail>
2025-08-11 16:02:42 +0300

    (be): initial setup for backend

---
+++ codepath-api/src/main/java/com/roberteftene/codepath/codepath_api/CodepathApiApplication.java
@@ -0,0 +1,13 @@
+package com.roberteftene.codepath.codepath_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepathApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodepathApiApplication.class, args);
+	}
+
+}
---
+++ codepath-api/src/test/java/com/roberteftene/codepath/codepath_api/CodepathApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberteftene.codepath.codepath_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodepathApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
